3cd69e2df0feedc1f4ba6c290f310026607ef17a
songdingxx <songdingxx@gmail.com>
2021-05-25 00:51:31 -0400

    update list code

--- list/list.h
+++ list/list.h
@@ -1,6 +1,8 @@
 #include "listNode.h"
+#include <cstdlib>
 
 template <typename T> class List
+// header -> first -> last -> trailer
 {
 private:
     // Size, head node, tail node
@@ -37,9 +39,9 @@ public:
     bool empty() const {return _size <= 0;}
     // Re-write [] operator to support access by index
     T& operator[] (Rank r) const;
-    // Get first node
+    // Get first node's pointer
     ListNodePosi(T) first() const {return header->succ;}
-    // Get last node
+    // Get last node's pointer
     ListNodePosi(T) last() const {return trailer->pred;}
     // judge whether is valid
     bool valid(ListNodePosi(T) p) {return p && (trailer != p) && (header != p);}
@@ -67,7 +69,7 @@ public:
     T remove(ListNodePosi(T) p);
     // Merge two list
     // TODO: why use l.first() and L._size as parameters here? Why not read these two variables in function?
-    void merge(List<T>& L) {merge(first(), size, L, L.first(), L_size);}
+    void merge(List<T>& L) {merge(first(), size, L, L.first(), L._size);}
     // sort within inteval n
     void sort(ListNodePosi(T) p, int n);
     // sort the whole list
@@ -87,3 +89,225 @@ public:
     template <typename VST>
     void traverse(VST&);
 };
+
+// Init funciton
+template <typename T> void List<T>::init()
+{
+    header = new ListNode<T>;
+    trailer - new ListNode<T>;
+    header->succ = trailer; header->pred = NULL;
+    trailer->pred = header; trailer->succ = NULL;
+    _size = 0;
+}
+
+// Rewrite [] operator
+template <typename T>
+T& List<T>::operator[] (Rank r) const
+{
+    ListNodePosi(T) p = first();
+    while(0 < r--) p = p->succ;
+    return p->data;
+}
+
+// search
+// unsorted list
+// before node p
+template <typename T>
+ListNodePosi(T) List<T>::find(T const& e, int n, ListNodePosi(T) p) const
+{
+    while(0 < n--)
+        if (e == (p = p->pred)->data) return p;
+    return NULL;
+}
+
+// sorted list
+template <typename T>
+ListNodePosi(T) List<T>::search(T const& e, int n, ListNodePosi(T) p) const
+{
+    while (0 <= n--)
+        if ((p = p->pred)->data <= e) break;
+    return p;
+}
+
+// insertion
+template <typename T> ListNodePosi(T) List<T>::insertAsFirst (T const& e)
+{
+    _size++; return header->insertAsSucc(e);
+}
+
+template <typename T> ListNodePosi(T) List<T>::insertAsLast(T const& e)
+{
+    _size++; return trailer->insertAsPred(e);
+}
+
+template <typename T> ListNodePosi(T) List<T>::insertA(ListNodePosi(T) p, T const& e)
+{
+    _size++; return p->insertAsSucc(e);
+}
+
+template <typename T> ListNodePosi(T) List<T>::insertB(ListNodePosi(T) p, T const& e)
+{
+    _size++; return p->insertAsPred(e);
+}
+
+// copynodes
+template<typename T>
+void List<T>::copyNodes(ListNodePosi(T) p, int n)
+{
+    init();
+    while (n--) {insertAsLast(p->data); p = p->succ;}
+}
+
+// Init list using copy
+template <typename T>
+List<T>::List(ListNodePosi(T) p, int n) {copyNodes(p, n);}
+
+// copy the whole list
+template <typename T>
+List<T>::List(List<T> const& L) {copyNodes(L.first(), L._size);}
+
+// copy from node r
+template <typename T>
+List<T>::List(List<T> const& L, int r, int n) {copyNodes(L[r], n);}
+
+// Delete
+template <typename T> 
+T List<T>::remove(ListNodePosi(T) p)
+{
+    T e = p->data;
+    p->pred->succ = p->succ; p->succ->pred = p->pred;
+    delete p; _size--;
+    return e;
+}
+
+// Deconstruction
+template <typename T>
+List<T>::~List() {clear(); delete header; delete trailer;}
+
+template <typename T> 
+int List<T>::clear()
+{
+    int oldSize = _size;
+    while (0 < _size) remove(header -> succ);
+    return oldSize;
+}
+
+// remove duplicated elements
+// unsorted list
+template <typename T>
+int List<T>::deduplicate()
+{
+    if (_size < 2) return 0;
+    int oldSize = _size;
+    ListNodePosi(T) p = header; Rank r = 0;
+    while (trailer != (p = p->succ))
+    {
+        ListNodePosi(T) q = find(p->data, r, p);
+        q ? remove(q) : r++;
+    }
+    return oldSize - _size;
+}
+
+// sorted list
+template <typename T>
+int List<T>::uniquify()
+{
+    if (_size < 2) return 0;
+    int oldSize = _size;
+    ListNodePosi(T) p = first(); ListNodePosi(T) q;
+    while (trailer != (q = p->succ))
+    {
+        if (p->data != q->data) p = q;
+        else remove(q);
+    }
+    return oldSize - _size;
+}
+
+// Traverse
+// use function's pointer?
+template <typename T>
+void List<T>::traverse(void (*visit) (T&))
+{
+    for (ListNodePosi(T) p = header->succ; p != trailer; p = p->succ) visit(p->data);
+}
+
+// Use reference
+template <typename T> template <typename VST>
+void List<T>::traverse(VST& visit)
+{
+    for (ListNodePosi(T) p = header->succ; p != trailer; p = p->succ) visit(p->data);
+}
+
+// Sort
+template <typename T>
+void List<T>::sort(ListNodePosi(T) p, int n)
+{
+    switch (std::rand() % 3)
+    {
+        case 1: insertionSort(p, n); break;
+        case 2: selectionSort(p, n); break;
+        default: mergeSort(p, n); break;
+    }
+}
+
+// Insertion sort
+template <typename T>
+void List<T>::insertionSort(ListNodePosi(T) p, int n)
+{
+    for (int r = 0; r < n; r++)
+    {
+        insertA(serach(p->data, r, p), p->data);
+        p = p->succ; remove(p->pred);
+    }
+}
+
+// Selection sort
+template <typename T>
+void List<T>::selectionSort(ListNodePosi(T) p, int n)
+{
+    ListNodePosi(T) head = p->pred; ListNodePosi(T) tail = p;
+    ListNodePosi(T) max = selectMax(head->succ, n);
+    insertB(tail, remove(max));
+    tail = tail->pred; n--;
+}
+
+template <typename T>
+ListNodePosi(T) List<T>::selectMax(ListNodePosi(T) p, int n)
+{
+    ListNodePosi(T) max = p;
+    for (ListNodePosi(T) cur = p; 1 < n; n--)
+    {
+        if (!lt((cur = cur->succ)->data, max->data)) max = cur;
+    }
+    return max;
+}
+
+// Merge sort
+// Use pointer's reference to mark the start of intervals - bravo! so clever.
+template <typename T>
+void List<T>::merge(ListNodePosi(T)& p, int n, List<T>& L, ListNodePosi(T) q, int m)
+{
+    ListNodePosi(T) pp = p->pred;
+    while (0 < m)
+    {
+        if ((0 < n) && (p->data <= q->data))
+        {
+            if (q == (p = p->succ)) break; n--;
+        }
+        else
+        {
+            insertB(p, L.remove((q = q->succ)->pred)); m--;
+        }
+    }
+    p = pp->succ;
+}
+
+template <typename T>
+void List<T>::mergeSort(ListNodePosi(T)& p, int n)
+{
+    if (n < 2) return;
+    int m = n >> 1;
+    ListNodePosi(T) q = p; for (int i = 0; i < m; i++) q = q->succ;
+    mergeSort(p, m); mergeSort(q, n-m);
+    merge(p, m, *this, q, n-m);
+}
--- list/listNode.h
+++ list/listNode.h
@@ -17,3 +17,19 @@ template <typename T> struct ListNode
     ListNodePosi(T) insertAsPred(T const& e);
     ListNodePosi(T) insertAsSucc(T const& e);
 };
+
+template <typename T>
+ListNodePosi(T) ListNode<T>::insertAsPred(T const& e)
+{
+    ListNodePosi(T) x = new ListNode(e, pred, this);
+    pred->succ = x; pred = x;
+    return x;
+}
+
+template <typename T>
+ListNodePosi(T) ListNode<T>::insertAsSucc(T const& e)
+{
+    ListNodePosi(T) x = new ListNode(e, this, succ);
+    succ->pred = x; succ = x;
+    return x;
+}
